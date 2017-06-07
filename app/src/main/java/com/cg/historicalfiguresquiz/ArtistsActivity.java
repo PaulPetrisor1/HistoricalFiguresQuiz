da008a08dc54fa3264f4939b0925da203e43bcfe
Paul Petrisor <PetrisorPaulRazvan@gmail.com>
2017-06-07 21:16:20 +0100

    Artists activity updates

--- app/src/main/java/com/cg/historicalfiguresquiz/ArtistsActivity.java
+++ app/src/main/java/com/cg/historicalfiguresquiz/ArtistsActivity.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 
 public class ArtistsActivity extends AppCompatActivity {
 
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
